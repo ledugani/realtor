8fcbdbdf772db0ec1d9fee494aec2b0d0de50b31
Tom Dugan <thomas@aadwebb.com>
2018-06-26 20:38:06 -0500

    updated constants and connections

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import connection from '../firebaseRequests/connection';
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
 import './App.css';
 
 class App extends Component {
+  componentDidMount () {
+    connection();
+  }
   render () {
     return (
       <div className="App">
