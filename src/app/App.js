961c157493bd03b3638e5be5e4b848dcfaa55dd1
Tom Dugan <thomas@aadwebb.com>
2018-06-26 20:57:52 -0500

    displays listings data in console

--- src/app/App.js
+++ src/app/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
+
 import connection from '../firebaseRequests/connection';
+import listingRequest from '../firebaseRequests/listings';
+
 import Listings from '../components/Listings/Listings';
 import Building from '../components/Building/Building';
 import ListingForm from '../components/ListingForm/ListingForm';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    listings: [],
+  }
+  // 2.)
   componentDidMount () {
     connection();
+
+    // 3.)
+    listingRequest.getRequest()
+      .then((listings) => {
+        this.setState({listings: listings}); // could just put listings
+      })
+      .catch((errror) => {
+        console.error('error with listing GET', errror);
+      });
   }
+  // 1.)
   render () {
     return (
       <div className="App">
